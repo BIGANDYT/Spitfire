cb618a1ed14bb65b8c88466de3878833c9eba97d
BIGANDYT <andyjt@gmail.com>
2015-11-25 21:05:10 +0000

    at utilties design done

---
+++ src/Framework/SitecoreExtensions/Rules/Conditions/HasCampaignEverTriggered.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Analytics.Core;
+using Sitecore.Analytics.Data;
+using Sitecore.Analytics.Model;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Habitat.SitecoreExtensions.Rules.Conditions
+{
+    public class HasCampaignEverTriggered<T> : WhenCondition<T>
+    where T : RuleContext
+    {
+        public string CampaignId
+        {
+            get;
+            set;
+        }
+
+        public HasCampaignEverTriggered()
+        {
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            Tracker.Current.Contact.LoadHistorycalData(4);
+            int points = Tracker.Current.Contact.System.Value;
+            bool flag = Tracker.Current.Interaction.GetPages().Any<IPageContext>((IPageContext x) => x.PageEvents.FirstOrDefault<Sitecore.Analytics.Model.PageEventData>((Sitecore.Analytics.Model.PageEventData y) => string.Equals(y.Data, this.CampaignId, StringComparison.InvariantCultureIgnoreCase)) != null);
+            return flag;
+        }
+    }
+}
