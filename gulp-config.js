bcaa229a4cb8a1908f232b219e4ef7cb10ef2cef
BIGANDYT <andyjt@gmail.com>
2016-07-18 12:07:44 +0100

    attempt to fix build

--- gulp-config.js
+++ gulp-config.js
@@ -1,10 +1,12 @@
-module.exports = function() {
-    var config = {
-        websiteRoot: "C:\\websites\\habitat.jango\\Website",
-        sitecoreLibraries: "C:\\websites\\habitat.jango\\Website\\bin",
-        solutionName: "habitat.jango",
+module.exports = function () {
+  var instanceRoot = "C:\\websites\\Habitat.Jango";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "habitat.jango",
     buildConfiguration: "Debug",
     runCleanBuilds: false
-  }
+  };
   return config;
 }
